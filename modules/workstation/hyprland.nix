61a96d6680ee86266c3a0d9536baa9c76d01692d
Luca Eggenberg <luca.e@outlook.com>
2025-07-31 23:15:21 +0200

    enable gnome keyrings

--- modules/workstation/hyprland.nix
+++ modules/workstation/hyprland.nix
@@ -2,6 +2,8 @@
     security.polkit.enable = true;
     programs.hyprland.enable = true;
     
+    services.gnome.gnome-keyring.enable = true;
+
     environment.systemPackages = with pkgs; [
         kitty
         mako
