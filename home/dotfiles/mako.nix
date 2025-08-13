6f010c2fdcd92e5ac8ce23923c8d8485cedc74fb
Luca Eggenberg <luca.e@outlook.com>
2025-08-13 14:47:11 +0200

    fix warnings

--- home/dotfiles/mako.nix
+++ home/dotfiles/mako.nix
@@ -6,17 +6,19 @@
     services.mako = {
         enable = true;
 
-        anchor = "top-right";
-        font = "monospace 10";
-        backgroundColor = "#303446";
-        textColor = "#c6d0f5";
-        width = 350;
-        margin = "20,20,0,0";
-        padding = 10;
-        borderSize = 2;
-        borderColor = "#737994";
-        borderRadius = 5;
-        defaultTimeout = 10000;
-        groupBy = "summary";
+        settings = {
+            anchor = "top-right";
+            font = "monospace 10";
+            backgroundColor = "#303446";
+            textColor = "#c6d0f5";
+            width = 350;
+            margin = "20,20,0,0";
+            padding = 10;
+            borderSize = 2;
+            borderColor = "#737994";
+            borderRadius = 5;
+            defaultTimeout = 10000;
+            groupBy = "summary";
+        };
     };
 }
