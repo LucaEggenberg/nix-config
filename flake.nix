5d1190f6fbfa349c00ec2afa115dcba4a42c291c
= <luca.e@outlook.com>
2026-06-02 21:28:04 +0200

    update system

--- flake.nix
+++ flake.nix
@@ -2,9 +2,9 @@
     description = "My personal nix config";
 
     inputs = {
-        nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+        nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
         home-manager = {
-            url = "github:nix-community/home-manager/release-25.11";
+            url = "github:nix-community/home-manager/release-26.05";
             inputs.nixpkgs.follows = "nixpkgs";
         };
         quickshell = {
@@ -27,7 +27,7 @@
 
     outputs = inputs@{ self, nixpkgs, ... }:
     let
-        version = "25.11";
+        version = "26.05";
 
         user = {
             userName = "luca";
